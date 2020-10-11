7ef74685ee62007e1cc3df59866a1a1b82f00345
GuardJo <dagh1218@naver.com>
2020-10-11 15:23:08 +0900

    chapter011~015 complete

---
+++ part01/012.js
@@ -0,0 +1,15 @@
+var subject = '자바스크립트';
+switch (subject) {
+    case 'C언어':
+        console.log("C언어 초보");
+        break;
+    case '자바스크립트':
+        console.log("자바스크립트 초보");
+        break;
+    case '파이썬' :
+        console.log("파이썬 초보");
+        break;
+    default:
+        console.log("초보가 아닙니다.");
+        break;
+}
---
+++ part01/014.js
@@ -0,0 +1,12 @@
+var store = {
+    snack : 1000,
+    flower : 5000,
+    beverage : 2000
+};
+
+for (var item in store) {
+    if (!store.hasOwnProperty(item)) {
+        continue;
+    }
+    console.log(item + " : " + store[item]);
+}
